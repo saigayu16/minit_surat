ffaab5a25a3b03f9477d7a5da17f049666ac28a8
saigayu16 <saigayu1605@gmail.com>
2026-07-07 10:48:36 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,13 @@
 <?php
-// IMPORTANT: NO spaces or lines before the <?php tag!
 $host = getenv('mysql-aae9a8-minitsurat123.c.aivencloud.com');
 $user = getenv('avnadmin');
-$pass = getenv('22408');
+$pass = getenv('DB_PASSWORD'); // This calls the value from the Render Dashboard
 $db   = getenv('defaultdb');
+$port = getenv('22408');
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// Do not have any echo or print statements here!
 ?>
-
