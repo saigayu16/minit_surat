1c7e819be0a7cf4adf7ee5a3f2e64519f0809a7f
saigayu16 <saigayu1605@gmail.com>
2026-07-07 10:40:14 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,15 @@
 <?php
-$host = "mysql-aae9a8-minitsurat123.c.aivencloud.com"; 
-$user = "avnadmin";
-// "DB_PASSWORD" is the NAME of the variable, not the password itself
-$pass = getenv("DB_PASSWORD"); 
-$dbname = "defaultdb";
-$port = 22408;
+// IMPORTANT: NO spaces or lines before the <?php tag!
+$host = getenv('mysql-aae9a8-minitsurat123.c.aivencloud.com');
+$user = getenv('avnadmin');
+$pass = getenv('22408');
+$db   = getenv('defaultdb');
 
-$conn = new mysqli($host, $user, $pass, $dbname, $port);
+$conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Sambungan database gagal: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
+// Do not have any echo or print statements here!
 ?>
 
