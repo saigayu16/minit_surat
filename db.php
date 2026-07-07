9bbcb68f1e5ec9f7953bc178e5a3fdd58fc75dbd
saigayu16 <saigayu1605@gmail.com>
2026-07-07 10:57:28 +0800

    Update db.php

--- db.php
+++ db.php
@@ -4,6 +4,10 @@ $user = getenv('avnadmin');
 $pass = getenv('DB_PASSWORD'); // This calls the value from the Render Dashboard
 $db   = getenv('defaultdb');
 $port = getenv('22408');
+// Ensure $host is not null/empty
+if (!$host) {
+    die("Error: DB_HOST is not set!");
+}
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
