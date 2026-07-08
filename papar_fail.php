0d6816ec940720d590351926f6903ab0e74a76e5
saigayu16 <saigayu1605@gmail.com>
2026-07-08 11:55:56 +0800

    Update papar_fail.php

--- papar_fail.php
+++ papar_fail.php
@@ -1,16 +1,15 @@
 <?php
-// papar_fail.php
 include('db.php');
-if (!isset($_GET['id'])) { die("ID tiada"); }
+if (!isset($_GET['id'])) { die("ID tidak dijumpai."); }
 
 $id = intval($_GET['id']);
-// Pastikan nama column 'fail_surat' adalah betul (jenis LONGBLOB)
 $res = $conn->query("SELECT fail_surat FROM minit_surat WHERE id = $id");
+$row = $res->fetch_assoc();
 
-if ($row = $res->fetch_assoc()) {
+if ($row && !empty($row['fail_surat'])) {
     header("Content-Type: application/pdf");
     echo $row['fail_surat']; 
 } else {
-    echo "Fail tidak dijumpai.";
+    echo "Fail tidak dijumpai atau kosong.";
 }
 ?>
