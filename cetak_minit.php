a5483f8a046204a812d05cc975b96dbcb47ac386
saigayu16 <saigayu1605@gmail.com>
2026-07-08 12:18:52 +0800

    Update cetak_minit.php

--- cetak_minit.php
+++ cetak_minit.php
@@ -121,17 +121,17 @@ $fail_path = 'uploads/' . htmlspecialchars($row['fail_surat'] ?? '');
 </div>
 
     <div class="footer-signature">
-        <?php if (strcasecmp($status, 'SELESAI') == 0 && !empty($signature_img)): ?>
-            <div class="sig-box">
-                <img src="<?= $signature_img ?>" style="max-height: 45px; width: auto;">
-                <div style="font-size: 10px; margin-top: 10px; border-top: 1px solid #cbd5e1; padding-top: 5px;">
-                    <b>PENGARAH</b><br><?= $tarikh_sah ?>
-                </div>
+    <?php if (strcasecmp($status, 'SELESAI') == 0 && !empty($row['tandatangan_data'])): ?>
+        <div class="sig-box">
+            <img src="data:image/png;base64,<?= base64_encode($row['tandatangan_data']) ?>" style="max-height: 45px; width: auto;">
+            <div style="font-size: 10px; margin-top: 10px; border-top: 1px solid #cbd5e1; padding-top: 5px;">
+                <b>PENGARAH</b><br><?= $tarikh_sah ?>
             </div>
-        <?php else: ?>
-            <div style="color: #94a3b8; font-style: italic;">(Menunggu tandatangan pengarah)</div>
-        <?php endif; ?>
-    </div>
+        </div>
+    <?php else: ?>
+        <div style="color: #94a3b8; font-style: italic;">(Menunggu tandatangan pengarah)</div>
+    <?php endif; ?>
+</div>
 </div>
 
 <button class="btn-print no-print" onclick="window.print()">
