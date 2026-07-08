f1fe745cbbef5bb0f09b859d9fe1c8161415a9b9
saigayu16 <saigayu1605@gmail.com>
2026-07-08 12:10:46 +0800

    Update papar_fail.php

--- papar_fail.php
+++ papar_fail.php
@@ -1,6 +1,6 @@
 <?php
 include('db.php');
-if (!isset($_GET['id'])) { die("ID tidak dijumpai."); }
+if (!isset($_GET['id'])) { die("ID tiada."); }
 
 $id = intval($_GET['id']);
 $res = $conn->query("SELECT fail_surat FROM minit_surat WHERE id = $id");
@@ -10,6 +10,6 @@ if ($row && !empty($row['fail_surat'])) {
     header("Content-Type: application/pdf");
     echo $row['fail_surat']; 
 } else {
-    echo "Fail tidak dijumpai atau kosong.";
+    echo "Dokumen tidak dijumpai.";
 }
 ?>
