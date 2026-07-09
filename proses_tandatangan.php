3fe8b1ccf9f81304cd08ced0b643ee9441ac788a
saigayu16 <saigayu1605@gmail.com>
2026-07-09 10:32:32 +0800

    Update proses_tandatangan.php

--- proses_tandatangan.php
+++ proses_tandatangan.php
@@ -1,32 +1,17 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // URL Web App Google
     $webAppUrl = 'https://script.google.com/macros/s/AKfycbyzLXkuCO7HCif_ESNPv8a96qwdW9v9zPCUSICJ9CKm_uPnAYStDBGgncZEsoGNQDEY/exec'; 
-
-    $payload = [
-        "id" => $_POST['id'],
-        "image" => $_POST['image'],
-        "fileId" => $_POST['fileId'],
-        "catatan" => $_POST['catatan'],
-        "arahan" => $_POST['arahan_pilihan'],
-        "folderId" => '1jXktGUFE2kZ32_LSk9DuybBsdXel6dL1'
-    ];
 
     $ch = curl_init($webAppUrl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // TAMBAH INI: Membenarkan redirect Google
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($_POST));
     curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
     
     $response = curl_exec($ch);
-    
-    // Semak jika ada error cURL
-    if (curl_errno($ch)) {
-        echo 'Error cURL: ' . curl_error($ch);
-    } else {
-        echo (trim($response) === 'SUCCESS') ? 'success' : 'Ralat Google: ' . $response;
-    }
-    
+    echo ($response === 'SUCCESS') ? 'success' : 'Ralat: ' . $response;
     curl_close($ch);
 }
 ?>
