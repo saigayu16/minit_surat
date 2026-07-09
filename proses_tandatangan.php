b66c3ba8b6806214fd152a1f96e75a535d7b4232
saigayu16 <saigayu1605@gmail.com>
2026-07-09 12:33:21 +0800

    Update proses_tandatangan.php

--- proses_tandatangan.php
+++ proses_tandatangan.php
@@ -3,24 +3,22 @@ session_start();
 include('db.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Gunakan $_POST seperti biasa kerana FormData menghantarnya sebagai POST
     $id = intval($_POST['id']);
     $catatan = $_POST['catatan'];
     $arahan = $_POST['arahan_pilihan'];
 
-    // 1. Update Database dahulu
+    // Update Database
     $stmt = $conn->prepare("UPDATE minit_surat SET status = 'SELESAI', catatan_pengarah = ?, arahan_pengarah = ? WHERE id = ?");
     $stmt->bind_param("ssi", $catatan, $arahan, $id);
     $stmt->execute();
 
-    // 2. Hantar ke Google Drive
+    // Hantar ke Google
     $webAppUrl = 'https://script.google.com/macros/s/AKfycbyzLXkuCO7HCif_ESNPv8a96qwdW9v9zPCUSICJ9CKm_uPnAYStDBGgncZEsoGNQDEY/exec'; 
     $ch = curl_init($webAppUrl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($_POST)); // Gunakan ini untuk FormData
-    
-    $response = curl_exec($ch);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($_POST));
+    curl_exec($ch);
     curl_close($ch);
 
     echo 'success';
