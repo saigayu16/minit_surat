56716fe1b32bed745b78a661501db1f6fe905726
saigayu16 <saigayu1605@gmail.com>
2026-07-10 09:15:31 +0800

    Update papar_dokumen.php

--- papar_dokumen.php
+++ papar_dokumen.php
@@ -1,21 +1,14 @@
 <?php
-// papar_dokumen.php
 include('db.php');
-session_start();
-
 if (!isset($_GET['id'])) die("Akses dinafikan.");
-
 $id = intval($_GET['id']);
 $query = $conn->query("SELECT fail_surat FROM minit_surat WHERE id = $id");
 $row = $query->fetch_assoc();
-
 if ($row) {
     $file_path = "uploads/" . $row['fail_surat'];
     if (file_exists($file_path)) {
         header('Content-Type: application/pdf');
         readfile($file_path);
-    } else {
-        echo "Fail tidak dijumpai.";
     }
 }
 ?>
